3365fc452fbdc813420b4307804270a89f372a2c
James Tipping <jamestipping@Jamess-MacBook-Pro.local>
2023-02-21 04:08:33 +0000

    Milestone 2 task 4 - check that input is a single character and in the alphabet

--- milestone_2.py
+++ milestone_2.py
@@ -7,3 +7,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Choose any letter from the alphabet and type it in").lower()
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input")
