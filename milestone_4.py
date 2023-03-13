5ca86533face72fb8c00937d9605adc999f647db
James Tipping <jamestipping@Jamess-MacBook-Pro.local>
2023-03-13 23:28:33 +0000

    Milestone 4 task 2

--- milestone_4.py
+++ milestone_4.py
@@ -9,4 +9,22 @@ class Hangman:
         self.num_lives = num_lives
         self.list_of_guesses: list[str] = []
 
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word')
+        
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter")
+
+            if guess.isalpha() == False:
+                print('Invalid guess. Please, enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter!')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
 
