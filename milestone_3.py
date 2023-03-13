1694906a21066b8f9850f3c57e3b6a7927f9b48d
James Tipping <jamestipping@Jamess-MacBook-Pro.local>
2023-03-13 22:33:44 +0000

    Milestone 3 task 1

--- milestone_3.py
+++ milestone_3.py
@@ -1,26 +1,8 @@
-import random
 
-word_list = ['banana', 'apple', 'grapes', 'strawberries', 'tomato']
-word = random.choice(word_list)
 
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.")
-        return True
-    else:
-        print(f"Sorry, {guess} is not in the word. Try again")
-        return False
-
-def ask_for_input():
-    guess = input("Guess any letter: ")
-
-    if guess.isalpha() == True and len(guess) == 1:
-        check = check_guess(guess)
-        if check == False:
-            ask_for_input()
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-        ask_for_input()
-
-ask_for_input()
+while True:
+    guess: str = input('Guess a letter')
+    if guess.isalpha():
+        break
+    else: 
+        print('Invalid letter. Please, enter a single alphabetical character')
